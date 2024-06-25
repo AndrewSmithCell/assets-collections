c03bad6f2e86540e06b36c9a33934ae4917814d0
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-06-25 14:55:24 +0900

    Update download-vsix.ps1

--- download-vsix.ps1
+++ download-vsix.ps1
@@ -1,57 +1,3 @@
-Function Get-VSMarketPlaceExtension {
-    [CmdLetBinding()]
-    Param(
-        [Parameter(ValueFromPipeline = $true,Mandatory = $true)]
-        [string[]]
-        $extensionName
-    )
-    begin {
-        $body=@{
-            filters = ,@{
-            criteria =,@{
-                    filterType=7
-                    value = $null
-                }
-            }
-            flags = 1712
-        }
-    }
-    process {
-        foreach($Extension in $extensionName) {
-            write-verbose "getting $($extensionName)"
-            $response =  try {
-                $body.filters[0].criteria[0].value = $Extension
-                $Query =  $body|ConvertTo-JSON -Depth 4
-                (Invoke-WebRequest -Uri "https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version=6.0-preview" -ErrorAction Stop -Body $Query -Method Post -ContentType "application/json")
-            } catch [System.Net.WebException] {
-                Write-Verbose "An exception was caught: $($_.Exception.Message)"
-                $_.Exception.Response
-            }
-            $statusCodeInt = [int]$response.StatusCode
-
-            if ($statusCodeInt -ge 400) {
-                Write-Warning "API Error :  $($response.StatusDescription)"
-                return
-            }
-            $ObjResults = ($response.Content | ConvertFrom-Json).results
-
-            If ($ObjResults.resultMetadata.metadataItems.count -ne 1) {
-                Write-Warning "Extension not found"
-                return
-            }
-
-            $extension = $ObjResults.extensions
-
-            $publisher = $extension.publisher.publisherName
-            $extensionName = $extension.extensionName
-            $version = $extension.versions[0].version
-
-            $uri = "$($extension.Versions[0].assetUri)/Microsoft.VisualStudio.Services.VSIXPackage"
-            Invoke-WebRequest -uri $uri -OutFile (Join-Path "$publisher.$extensionName.$version.VSIX")
-        }
-    }
-}
-
 $extensions = @(
     "cweijan.vscode-autohotkey-plus";
     "stylelint.vscode-stylelint";
@@ -80,5 +26,45 @@ $extensions = @(
     "ms-python.black-formatter";
     "ms-python.autopep8";
 )
+$body=@{
+    filters = ,@{
+    criteria =,@{
+            filterType=7
+            value = $null
+        }
+    }
+    flags = 1712
+}
+foreach($Extension in $extensions) {
+    write-output "getting $($Extension)"
+    $response =  try {
+        $body.filters[0].criteria[0].value = $Extension
+        $Query =  $body|ConvertTo-JSON -Depth 4
+        (Invoke-WebRequest -Uri "https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version=6.0-preview" -ErrorAction Stop -Body $Query -Method Post -ContentType "application/json")
+    } catch [System.Net.WebException] {
+        Write-output "An exception was caught: $($_.Exception.Message)"
+        $_.Exception.Response
+    }
+    $statusCodeInt = [int]$response.StatusCode
 
-$extensions | Get-VSMarketPlaceExtension
+    if ($statusCodeInt -ge 400) {
+        Write-Warning "API Error :  $($response.StatusDescription)"
+        return
+    }
+    $ObjResults = ($response.Content | ConvertFrom-Json).results
+
+    If ($ObjResults.resultMetadata.metadataItems.count -ne 1) {
+        Write-Warning "Extension not found"
+        return
+    }
+
+    $extension = $ObjResults.extensions
+
+    $publisher = $extension.publisher.publisherName
+    $extensionName = $extension.extensionName
+    $version = $extension.versions[0].version
+
+    $uri = "$($extension.Versions[0].assetUri)/Microsoft.VisualStudio.Services.VSIXPackage"
+    Invoke-WebRequest -uri $uri -OutFile (Join-Path "$publisher.$extensionName.$version.VSIX")
+}
+
