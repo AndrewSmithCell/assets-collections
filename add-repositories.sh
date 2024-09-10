ceb9acd79d20a4fca25a5bee921341e0a89735d2
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-09-11 08:47:02 +0900

    Update add-repositories.sh

--- add-repositories.sh
+++ add-repositories.sh
@@ -1,9 +1,8 @@
 apt-get update
-sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
 apt-get install -y ca-certificates curl gnupg
 curl -fsSL https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor -o /etc/apt/keyrings/adoptium.gpg
 cat > /etc/apt/sources.list.d/adoptium.sources <<EOF
-Types: deb
+Types: deb deb-src
 URIs: https://packages.adoptium.net/artifactory/deb
 Suites: bookworm
 Components: main
@@ -11,7 +10,7 @@ Signed-By: /etc/apt/keyrings/adoptium.gpg
 EOF
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 cat > /etc/apt/sources.list.d/nodesource.sources <<EOF
-Types: deb
+Types: deb deb-src
 URIs: https://deb.nodesource.com/node_20.x
 Suites: nodistro
 Components: main
