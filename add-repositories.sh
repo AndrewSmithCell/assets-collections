6729e377959afa2388d14e1310c9e7fc63028f0d
AndrewSmithCell <AndrewSmithCell@outlook.com>
2024-09-11 08:49:02 +0900

    Update add-repositories.sh

--- add-repositories.sh
+++ add-repositories.sh
@@ -1,19 +1,1 @@
 apt-get update
-apt-get install -y ca-certificates curl gnupg
-curl -fsSL https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor -o /etc/apt/keyrings/adoptium.gpg
-cat > /etc/apt/sources.list.d/adoptium.sources <<EOF
-Types: deb deb-src
-URIs: https://packages.adoptium.net/artifactory/deb
-Suites: bookworm
-Components: main
-Signed-By: /etc/apt/keyrings/adoptium.gpg
-EOF
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-cat > /etc/apt/sources.list.d/nodesource.sources <<EOF
-Types: deb deb-src
-URIs: https://deb.nodesource.com/node_20.x
-Suites: nodistro
-Components: main
-Signed-By:/etc/apt/keyrings/nodesource.gpg
-EOF
-sudo apt-get update        
