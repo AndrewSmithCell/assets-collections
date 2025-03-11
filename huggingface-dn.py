7e60f5d89206e08550549911f77ac4acde35c641
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-11 17:36:48 +0900

    Update huggingface-dn.py

--- huggingface-dn.py
+++ huggingface-dn.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
 
     home_dir = 'files'
     os.makedirs(home_dir)
-    layoutreader_model_dir = snapshot_download('microsoft/speecht5_hifigan', local_dir=home_dir, local_dir_use_symlinks=False,)
+    layoutreader_model_dir = snapshot_download('Matthijs/cmu-arctic-xvectors', local_dir=home_dir, local_dir_use_symlinks=False,)
     print(f'model_dir is: {layoutreader_model_dir}')
