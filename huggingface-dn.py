bc56a644a7cfd24fc55c0419b4b10ddcb1726cb6
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-03-11 12:34:59 +0900

    Update huggingface-dn.py

--- huggingface-dn.py
+++ huggingface-dn.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
 
     home_dir = 'files'
     os.makedirs(home_dir)
-    layoutreader_model_dir = snapshot_download('sentence-transformers/all-MiniLM-L6-v2', local_dir=home_dir, local_dir_use_symlinks=False,)
+    layoutreader_model_dir = snapshot_download('Systran/faster-whisper-base', local_dir=home_dir, local_dir_use_symlinks=False,)
     print(f'model_dir is: {layoutreader_model_dir}')
