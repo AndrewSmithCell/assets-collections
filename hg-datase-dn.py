e874f9352d27ef46e7339df484343cc81a3e1095
AndrewSmithCell <AndrewSmithCell@outlook.com>
2025-04-02 18:16:24 +0900

    Update hg-datase-dn.py

--- hg-datase-dn.py
+++ hg-datase-dn.py
@@ -1,5 +1,5 @@
 import datasets
 import os
-data = datasets.load_dataset("open-r1/codeforces-cots", split="validation")
+data = datasets.load_dataset("open-r1/codeforces-cots")
 os.makedirs("files", exist_ok=True)
 data.save_to_disk("files")
